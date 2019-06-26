b2f50cf685643c8df9ef738b646cf0af706dab1b
Alexs Gonzalez <unauthorize@fb.com>
2019-06-25 18:28:21 -0700

    Add Tipster to Github

---
+++ Tipster/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tipster
+//
+//  Created by unauthorize on 6/25/19.
+//  Copyright © 2019 unauthorize. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Tipster/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Tipster
+//
+//  Created by unauthorize on 6/25/19.
+//  Copyright © 2019 unauthorize. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
